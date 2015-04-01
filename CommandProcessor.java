6321c514d5d86ff3e1a821cdbfa5fd5d08c590de
luqmanarifin <l.arifin.siswanto@gmail.com>
2015-04-01 13:29:16 +0700

    Doc
    Baru

--- CommandProcessor.java
+++ CommandProcessor.java
@@ -2,8 +2,44 @@ public class CommandProcessor {
   public CommandProcessor() {
   
   }
+  public CommandProcessor(String str) {
+  
+  }
   public CommandProcessor(CommandProcessor com) {
   
+  }
+  public void undo(int n) {
+  
+  }
+  public void redo(int n) {
+  
+  }
+  public void setExpressionMode(int mode) {
+  
+  }
+  public void setEquationMode(int mode) {
+  
+  }
+  public void setNumberMode(int mode) {
+  
+  }
+  public void resetSetting() {
+  
+  }
+  public void showMem(int n) {
+  
+  }
+  public void showMemAll() {
+  
+  }
+  public void help() {
+  
+  }
+  public void viewSetting() {
+  
+  }
+  public boolean isCommand() {
+    
   }
   private Saver saver;
   private Logger logger;

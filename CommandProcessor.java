a51749825a08503145b47f8e2bc67b518e829598
luqmanarifin <l.arifin.siswanto@gmail.com>
2015-04-01 13:12:29 +0700

    Naming Convention
    Naming convention di project Java kita

---
+++ CommandProcessor.java
@@ -0,0 +1,10 @@
+public class CommandProcessor {
+  public CommandProcessor() {
+  
+  }
+  public CommandProcessor(CommandProcessor com) {
+  
+  }
+  private Saver saver;
+  private Logger logger;
+}
